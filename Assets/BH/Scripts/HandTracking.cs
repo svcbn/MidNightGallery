821df34bd7569e75c3b78a1d5c4ab37d652c9580
svcbn <qudgks7946@gmail.com>
2022-09-01 15:20:05 +0900

    asd
    asdf

--- Assets/BH/Scripts/HandTracking.cs
+++ Assets/BH/Scripts/HandTracking.cs
@@ -9,6 +9,9 @@ public class HandTracking : MonoBehaviour
     public GameObject[] handPoints;
 
     public GameObject hand;
+
+    public float speed = 20f;
+
     void Start()
     {
         hand = GameObject.Find("Hand");
@@ -24,6 +27,7 @@ public class HandTracking : MonoBehaviour
         print(data);
         string[] points = data.Split(',');
         print(points[0]);
+
  
         //0        1*3      2*3
         //x1,y1,z1,x2,y2,z2,x3,y3,z3
@@ -31,13 +35,12 @@ public class HandTracking : MonoBehaviour
         for ( int i = 0; i<21; i++)
         {
  
-            float x = 7-float.Parse(points[i * 3]) / 100;
+            float x = 9 - float.Parse(points[i * 3]) / 100;
             float y = float.Parse(points[i * 3 + 1]) / 100;
             float z = float.Parse(points[i * 3 + 2]) / 100;
- 
-            handPoints[i].transform.localPosition = new Vector3(x, y, 0);
-            hand.transform.position = new Vector3(0, 0, z);
- 
+
+            handPoints[i].transform.localPosition = Vector3.Lerp(handPoints[i].transform.localPosition, new Vector3(x, y, z), speed * Time.deltaTime);
+            //hand.transform.position = new Vector3(0, 0, z/2);
         }
  
  
