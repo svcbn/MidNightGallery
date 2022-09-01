7fadbe70b60bff2fa5213d21568d522fd7224167
svcbn <qudgks7946@gmail.com>
2022-09-01 23:22:25 +0900

    fd
    fds

--- Assets/BH/Scripts/FireScene.cs
+++ Assets/BH/Scripts/FireScene.cs
@@ -7,15 +7,18 @@ public class FireScene : MonoBehaviour
     // Start is called before the first frame update
 
     public List<GameObject> fires = new List<GameObject>();
+    public GameObject wrist;
 
     void Start()
     {
-        
+
     }
 
+    // -3.5 ~ 7
     // Update is called once per frame
     void Update()
     {
+        float x = wrist.transform.localPosition.x;
         
     }
 }
