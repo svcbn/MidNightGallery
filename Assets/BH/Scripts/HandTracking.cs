576a8da704182655613f3348ec48660196c5d07c
svcbn <104844841+svcbn@users.noreply.github.com>
2023-04-04 17:18:55 +0900

    Update HandTracking.cs

--- Assets/BH/Scripts/HandTracking.cs
+++ Assets/BH/Scripts/HandTracking.cs
@@ -11,10 +11,11 @@ public class HandTracking : MonoBehaviour
     public GameObject hand;
 
     public float speed = 20f;
-   public float adjuster = 100;
+    public float adjuster = 100;
 
-   public int[] fingerPoints= new int[5];
-   public string fingerPointS; 
+    public int[] fingerPoints= new int[5];
+    public string fingerPointS; 
+    
     void Start()
     {
         hand = GameObject.Find("Hand");
@@ -33,8 +34,7 @@ public class HandTracking : MonoBehaviour
 
         fingerPointS="";
         fingerPointS += points[0]+ (points[1])+ (points[2]) + (points[3]) + (points[4]);
-
- 
+        
         //0        1*3      2*3
         //x1,y1,z1,x2,y2,z2,x3,y3,z3
         fingerPoints[0] = int.Parse(points[0]);
@@ -44,15 +44,10 @@ public class HandTracking : MonoBehaviour
         fingerPoints[4] = int.Parse(points[4]);
  
         for ( int i = 0; i<21; i++)
-        {
-            // 6 7 8 
-            //손가락 값이 바뀔 때만 fingerPoint를 받는다
-
- 
+        { 
             float x = 9 - float.Parse(points[i * 3 + 5]) /adjuster;
             float y = float.Parse(points[i * 3 + 6]) / adjuster;
             float z = float.Parse(points[i * 3 + 7]) / adjuster;
-
 
             handPoints[i].transform.localPosition = Vector3.Lerp(handPoints[i].transform.localPosition, new Vector3(x, y, z), speed * Time.deltaTime);
             //hand.transform.position = new Vector3(0, 0, z/2);
